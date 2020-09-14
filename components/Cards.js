d95eea0713e392e295d39c381de08b13226c7ea7
ChadDiaz <chadcdiaz@gmail.com>
2020-09-13 23:21:45 -0400

    tried to work on 2 stretch goals.  Completed 1 stretch goal I think

--- components/Cards.js
+++ components/Cards.js
@@ -59,7 +59,7 @@ const cardWrap = document.createElement('div');
 return cardWrap;
 }
 
-axios.get('https://lambda-times-api.herokuapp.com/articles')
+axios.get('https://lambda-timeass-api.herokuapp.com/articles')
     .then((goodResponse) => {
         log('card response here' , goodResponse);
         const responseArray = Object.values(goodResponse.data.articles)
@@ -72,6 +72,5 @@ axios.get('https://lambda-times-api.herokuapp.com/articles')
                 })
             })
     })
-    .catch((badResponse) => {
-        log(badResponse);
+    .catch((badResponse) => {alert(`${badResponse} is why this page didn't load`);
     })
